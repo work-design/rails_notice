76ad8073c8dae6e5f61c7249cc7009b176a76b85
qinmingyuan <mingyuan0715@foxmail.com>
2017-01-10 11:58:30 +0800

    add concern

--- app/models/the_publishable.rb
+++ app/models/the_publishable.rb
@@ -1,4 +1,5 @@
 module ThePublishable
+  extend ActiveSupport::Concern
 
   included do
     Notification.belongs_to :publisher, class_name: name, foreign_key: 'publisher_id', optional: true, inverse_of: :notifications
--- app/models/the_receivable.rb
+++ app/models/the_receivable.rb
@@ -1,4 +1,5 @@
 module TheReceivable
+  extend ActiveSupport::Concern
 
   included do
     Notification.belongs_to :receiver, class_name: name, foreign_key: 'receiver_id', optional: true, inverse_of: :notifications
