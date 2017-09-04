57eafeeccf143f226b0268052bda5e3bc88d1397
qinmingyuan <mingyuan0715@foxmail.com>
2017-09-04 18:07:04 +0800

    comment

--- app/controllers/admin/the_notify_controller.rb
+++ app/controllers/admin/the_notify_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TheNotifyController < TheNotify.config.admin_class.constantize
-  skip_before_action :verify_authenticity_token#, if: -> { request.content_type == 'application/json' }
+  #skip_before_action :verify_authenticity_token#, if: -> { request.content_type == 'application/json' }
 
   def index
 
