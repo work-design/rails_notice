b1df35746b8a32645d015470dd8031ed3551ef5d
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-21 16:43:44 +0800

    notifiable optionalbe

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :receiver, polymorphic: true
-  belongs_to :notifiable, polymorphic: true
+  belongs_to :notifiable, polymorphic: true, optional: true
   has_one :notification_setting, ->(o) { where(receiver_type: o.receiver_type) }, primary_key: :receiver_id, foreign_key: :receiver_id
 
   default_scope -> { order(id: :desc) }
