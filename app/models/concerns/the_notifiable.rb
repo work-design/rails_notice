3b97fdb32dfa8bb6d4ee7e24ec64b5020af65645
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-10 17:05:02 +0800

    fix wrong beglong to

--- app/models/concerns/the_notifiable.rb
+++ app/models/concerns/the_notifiable.rb
@@ -4,7 +4,6 @@ module TheNotifiable
   included do
     delegate :url_helpers, to: 'Rails.application.routes'
 
-    Notification.belongs_to :publisher, class_name: name, foreign_key: 'publisher_id', optional: true, inverse_of: :notifications
     has_many :notifications, as: 'notifiable', dependent: :nullify
   end
 
