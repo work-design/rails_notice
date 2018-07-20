1270c5445100c34a0843498310eab9de45826676
qinmingyuan <mingyuan0715@foxmail.com>
2018-07-20 17:13:36 +0800

    no preview

--- lib/the_notify/engine.rb
+++ lib/the_notify/engine.rb
@@ -3,7 +3,6 @@ module TheNotify
 
     initializer 'the_notify.assets.precompile' do |app|
       app.config.assets.precompile += ['the_notify_manifest.js']
-      app.config.action_mailer.preview_path = File.expand_path('test/mailers/previews', root)
     end
 
   end
--- test/mailers/previews/the_notify_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# http://localhost:3000/rails/mailers/the_notify_mailer_preview
-class TheNotifyMailerPreview < ActionMailer::Preview
-
-  def notify
-    @notification = Notification.first
-    TheNotifyMailer.notify @notification.id
-  end
-
-end
