58a7cd70615f4411ff2c30ddeced0ab5a0a09add
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-17 17:37:09 +0800

    remove unued code

--- app/models/rails_notice/annunciation.rb
+++ app/models/rails_notice/annunciation.rb
@@ -10,20 +10,6 @@ class Annunciation < ApplicationRecord
     published: 'published'
   }
 
-  def do_trigger(params = {})
-    self.trigger_to state: params[:state]
-
-    self.class.transaction do
-      self.save!
-      to_notification(
-        receiver: self.verifier,
-        sender: self.publisher,
-        link: url_helpers.admin_app_url(id: self.id),
-        verbose: true
-      ) if self.verifier
-    end
-  end
-
   def to_notifications
     self.update(state: 'published')
     Notification.bulk_insert_from_model(
