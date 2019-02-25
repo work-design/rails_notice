e257fb529c131f3a0cb629cb655b351618f106c2
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-25 20:25:34 +0800

    use getui

--- app/models/concerns/rails_notice_getui.rb
+++ app/models/concerns/rails_notice_getui.rb
@@ -6,7 +6,9 @@ module RailsNoticeGetui
 
 
   def send_to_getui
-    message = Getui::Apns.new(self.body, self.title)
+    apns = Getui::Apns.new(self.body, title: self.title)
+    message = Getui::Message::Transmission.new('请填写透传内容')
+    message.apns = apns
 
     Getui.push_single(receiver.getui_token, message)
   end
