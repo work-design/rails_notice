6d1cb8ac509c25ebeefdf675b13bca06029e3e25
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-25 20:32:02 +0800

    removed code

--- lib/rails_notice.rb
+++ lib/rails_notice.rb
@@ -1,7 +1,6 @@
 require 'rails_notice/engine'
 require 'rails_notice/config'
 require 'rails_notice/active_record'
-require 'rails_notice/pusher/getui_helper'
 
 module RailsNotice
   mattr_writer :notifiable_types do
