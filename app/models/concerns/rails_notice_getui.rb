23fd4e469ead16cf2792ace74f10244c897d4d4e
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-27 16:00:45 +0800

    return unless getui token

--- app/models/concerns/rails_notice_getui.rb
+++ app/models/concerns/rails_notice_getui.rb
@@ -6,6 +6,7 @@ module RailsNoticeGetui
 
 
   def send_to_getui
+    return unless receiver.getui_token
     apns = Getui::Apns.new(self.body, title: self.title)
     message = Getui::Message::Transmission.new('请填写透传内容')
     message.apns = apns
