b2fb2daadddb4b4c948f7340015e2f7ca2798740
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-28 17:30:42 +0800

    notice getui

--- app/models/concerns/rails_notice_getui.rb
+++ app/models/concerns/rails_notice_getui.rb
@@ -1,9 +1,4 @@
 module RailsNoticeGetui
-  Getui.app_id = 'X1HEFkU8ID6p59ycdbql91'
-  Getui.app_key = 'NxWOzrhZ6j7O2AvE7w1Dq3'
-  Getui.master_secret = 'B3u6uQUsHx6FQ3Y8134r2A'
-
-
 
   def send_to_getui
     return unless receiver.getui_token
