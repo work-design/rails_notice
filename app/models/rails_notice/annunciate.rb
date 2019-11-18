a150c289e5326637fc8b555ff43a2e992862b6d2
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-18 16:28:07 +0800

    decrement

--- app/models/rails_notice/annunciate.rb
+++ app/models/rails_notice/annunciate.rb
@@ -9,12 +9,19 @@ module RailsNotice::Annunciate
     has_many :user_taggeds, foreign_key: :user_tag_id, primary_key: :user_tag_id
     has_many :notification_settings, through: :user_taggeds
     
-    after_create_commit :update_unread_count
+    after_create_commit :increment_unread_count
+    after_destroy_commit :decrement_unread_count
   end
   
   def increment_unread_count
     ['total', 'official', 'Annunciation'].each do |col|
       notification_settings.increment_unread_counter(col)
+    end
+  end
+  
+  def decrement_unread_count
+    ['total', 'official', 'Annunciation'].each do |col|
+      notification_settings.decrement_unread_counter(col)
     end
   end
   
