8ef3e6ac88d8590c0e4a19e57cced07d0c7dfa6a
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-19 19:20:10 +0800

    fix counter

--- app/models/rails_notice/annunciate.rb
+++ app/models/rails_notice/annunciate.rb
@@ -8,6 +8,7 @@ module RailsNotice::Annunciate
     belongs_to :user_tag, optional: true
     has_many :user_taggeds, foreign_key: :user_tag_id, primary_key: :user_tag_id
     has_many :notification_settings, through: :user_taggeds
+    has_many :annunciates, class_name: self.name, foreign_key: :annunciation_id, primary_key: :annunciation_id
     
     after_create :increment_unread_count
     after_destroy :decrement_unread_count
@@ -15,14 +16,20 @@ module RailsNotice::Annunciate
   
   def increment_unread_count
     ['total', 'official', 'Annunciation'].each do |col|
-      notification_settings.increment_unread_counter(col)
+      notification_settings.where(receiver_type: self.receiver_type).where.not(receiver_id: same_user_ids).increment_unread_counter(col)
     end
   end
   
   def decrement_unread_count
     ['total', 'official', 'Annunciation'].each do |col|
-      notification_settings.decrement_unread_counter(col)
+      notification_settings.where(receiver_type: self.receiver_type).where.not(receiver_id: same_user_ids).decrement_unread_counter(col)
     end
+  end
+  
+  # todo better sql
+  def same_user_ids
+    user_tag_ids = self.annunciates.pluck(:user_tag_id)
+    UserTagged.where(user_tag_id: user_tag_ids).having('COUNT(user_id) > 1').group(:user_id).count(:user_id).keys
   end
   
 end
