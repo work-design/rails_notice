ef6f8ff72a01c2f7674aab74201eb51b1f520852
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-30 21:04:55 +0800

    fix test

--- test/controllers/notice/admin/annunciates_controller_test.rb
+++ test/controllers/notice/admin/annunciates_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 class Notice::Admin::AnnunciatesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
-    @notice_admin_annunciate = create notice_admin_annunciates
+    @annunciate = create :annunciate
   end
 
   test 'index ok' do
@@ -24,23 +24,23 @@ class Notice::Admin::AnnunciatesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'show ok' do
-    get admin_annunciate_url(@notice_admin_annunciate)
+    get admin_annunciate_url(@annunciate)
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_admin_annunciate_url(@notice_admin_annunciate)
+    get edit_admin_annunciate_url(@annunciate)
     assert_response :success
   end
 
   test 'update ok' do
-    patch admin_annunciate_url(@notice_admin_annunciate), params: { #{singular_table_name}: { #{attributes_string} } }
+    patch admin_annunciate_url(@annunciate), params: { #{singular_table_name}: { #{attributes_string} } }
     assert_response :success
   end
 
   test 'destroy ok' do
     assert_difference('Annunciate.count', -1) do
-      delete admin_annunciate_url(@notice_admin_annunciate)
+      delete admin_annunciate_url(@annunciate)
     end
 
     assert_response :success
