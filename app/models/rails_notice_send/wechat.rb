7b922b766e4047bf327b233430223d227811e715
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-05 13:06:16 +0800

    wechat notcie related to notification

--- app/models/rails_notice_send/wechat.rb
+++ app/models/rails_notice_send/wechat.rb
@@ -8,7 +8,8 @@ module RailsNoticeSend::Wechat
   def send_out
     return unless wechat_template
     wechat_notice = wechat_template.wechat_notices.build
-    wechat_notice.wechat_user = receiver.wechat_users
+    wechat_notice.wechat_subscribed = receiver.wechat_subscribeds
+    wechat_notice.notifiaction = self
     wechat_notice.save
   end
 
