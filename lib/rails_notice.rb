d05bdc5b3d15603a6e1fa900cf605224482a8737
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-15 17:14:45 +0800

    rails notice

--- lib/rails_notice.rb
+++ lib/rails_notice.rb
@@ -1,12 +1,13 @@
 require 'rails_notice/engine'
 require 'rails_notice/config'
 require 'rails_notice/active_record'
+require 'rails_notice/i18n_helper'
 
 module RailsNotice
   mattr_accessor :notifiable_types, default: {}
-  
+
   def self.xxx
     notifiable_types.map { |i| i}
   end
-  
+
 end
