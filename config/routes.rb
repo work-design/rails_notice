7e1f7a0d73004f5e24b15a613f09b434c987157f
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-07 22:53:14 +0800

    add notice

--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,59 @@
 Rails.application.routes.draw do
 
-  scope 'notice/admin', module: 'notice/admin', defaults: { business: 'notice', namespace: 'admin' } do
-    resources :notifications do
-      member do
-        patch :push
-        patch :email
+  namespace :notice, defaults: { business: 'notice' } do
+    namespace :admin, defaults: { namespace: 'admin' } do
+      resources :notifications do
+        member do
+          patch :push
+          patch :email
+        end
       end
-    end
-    resources :notify_settings do
-      collection do
-        get :columns
+      resources :notify_settings do
+        collection do
+          get :columns
+        end
       end
-    end
-    resources :users
-    resources :members
-    resources :annunciations do
-      member do
-        get 'user' => :edit_user
-        patch 'user' => :update_user
-        get 'member' => :edit_member
-        patch 'member' => :update_member
-        get 'publish/options' => :options
+      resources :users
+      resources :members
+      resources :annunciations do
+        member do
+          get 'user' => :edit_user
+          patch 'user' => :update_user
+          get 'member' => :edit_member
+          patch 'member' => :update_member
+          get 'publish/options' => :options
+        end
       end
+      resources :annunciates
     end
-    resources :annunciates
-  end
 
-  scope :me, module: 'notice/me', as: :me, defaults: { business: 'notice', namespace: 'me' } do
-    resources :notifications, only: [:index, :show, :destroy] do
-      collection do
-        post :read_all
+    namespace :me, defaults: { namespace: 'me' } do
+      resources :notifications, only: [:index, :show, :destroy] do
+        collection do
+          post :read_all
+        end
+        member do
+          get :url
+          patch :read
+          match :archive, via: [:put, :patch]
+        end
       end
-      member do
-        get :url
-        patch :read
-        match :archive, via: [:put, :patch]
-      end
+      resource :notification_setting, only: [:show, :edit, :update]
     end
-    resource :notification_setting, only: [:show, :edit, :update]
-  end
 
-  scope :my, module: 'notice/my', as: :my, defaults: { business: 'notice', namespace: 'my' } do
-    resources :notifications, only: [:index, :show, :destroy] do
-      collection do
-        post :read_all
+    namespace :my, defaults: { namespace: 'my' } do
+      resources :notifications, only: [:index, :show, :destroy] do
+        collection do
+          post :read_all
+        end
+        member do
+          get :url
+          patch :read
+          match :archive, via: [:put, :patch]
+        end
       end
-      member do
-        get :url
-        patch :read
-        match :archive, via: [:put, :patch]
-      end
+      resource :notification_setting, only: [:show, :edit, :update]
     end
-    resource :notification_setting, only: [:show, :edit, :update]
   end
 
 end
