98f389f36bb4aad779ba6644d00a37c6c5ebfedb
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-05 09:41:04 +0800

    update gemfile

--- app/models/notice/model/annunciation/member_annunciation.rb
+++ app/models/notice/model/annunciation/member_annunciation.rb
@@ -3,7 +3,7 @@ module Notice
     extend ActiveSupport::Concern
 
     included do
-      has_many :annunciates, class_name: 'MemberAnnunciate', foreign_key: :annunciation_id, dependent: :destroy
+      has_many :annunciates, class_name: 'MemberAnnunciate', foreign_key: :annunciation_id, dependent: :destroy_async
       has_many :departments, class_name: 'Org::Department', through: :annunciates
       has_many :job_titles, class_name: 'Org::JobTitle', through: :annunciates
     end
--- app/models/notice/model/annunciation/user_annunciation.rb
+++ app/models/notice/model/annunciation/user_annunciation.rb
@@ -3,7 +3,7 @@ module Notice
     extend ActiveSupport::Concern
 
     included do
-      has_many :annunciates, class_name: 'UserAnnunciate', foreign_key: :annunciation_id, dependent: :destroy
+      has_many :annunciates, class_name: 'UserAnnunciate', foreign_key: :annunciation_id, dependent: :destroy_async
       has_many :user_tags, through: :annunciates
     end
 
