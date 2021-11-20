ea6073cfce9e885ab5051726fa3908c7b0d4284b
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-20 21:56:14 +0800

    notice

--- app/models/notice/send/wechat.rb
+++ app/models/notice/send/wechat.rb
@@ -6,13 +6,15 @@ module Notice
 
       return unless template_config
       user.wechat_users.default_where('app.organ_id': self.organ_id).map do |wechat_user|
+        wechat_template = wechat_user.app.templates.find_by(template_config_id: template_config.id)
+        next if wechat_template.nil?
+
         if wechat_user.app.is_a?(Wechat::PublicApp)
           wechat_notice = wechat_user.notices.build type: 'Wechat::PublicNotice'
         else
           wechat_notice = wechat_user.notices.build type: 'Wechat::ProgramNotice'
         end
 
-        wechat_template = wechat_user.app.templates.find_by(template_config_id: template_config.id)
         wechat_notice.template = wechat_template
         wechat_notice.msg_request = wechat_template.msg_requests.where(open_id: wechat_user.uid).first
         wechat_notice.notification = self
