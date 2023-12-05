ce217fd4b70d2550f608ec984a71cee77e8ffc30
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-05 11:02:41 +0800

    gem

--- app/models/notice/send/socket.rb
+++ app/models/notice/send/socket.rb
@@ -3,16 +3,26 @@ module Notice
 
     def send_out
       super if defined? super
-      return unless user
 
-      ReceiverChannel.broadcast_to(user_id, {
-        id: id,
-        body: body,
-        count: user.unread_count,
-        link: link,
-        showtime: user.showtime
-      })
-      self.notification_sendings.find_or_create_by(way: 'websocket', sent_to: user_id)
+      if user
+        ReceiverChannel.broadcast_to(user_id, {
+          id: id,
+          body: body,
+          count: user.unread_count,
+          link: link,
+          showtime: user.showtime
+        })
+        self.notification_sendings.find_or_create_by(way: 'websocket', sent_to: user_id)
+      elsif member
+        ReceiverChannel.broadcast_to(member.identity, {
+          id: id,
+          body: body,
+          count: member.unread_count,
+          link: link,
+          showtime: member.showtime
+        })
+        self.notification_sendings.find_or_create_by(way: 'websocket', sent_to: member_id)
+      end
     end
 
   end
