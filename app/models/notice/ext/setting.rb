550521bbd7ea69912a3845bc5992a8a1c8a083a0
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-07 17:40:24 +0800

    json

--- app/models/notice/ext/setting.rb
+++ app/models/notice/ext/setting.rb
@@ -3,11 +3,7 @@ module Notice
     extend ActiveSupport::Concern
 
     included do
-      if connection.adapter_name == 'PostgreSQL'
-        attribute :notifiable_types, :string, array: true, default: []
-      else
-        attribute :notifiable_types, :json
-      end
+      attribute :notifiable_types, :json, default: []
       attribute :counters, :json, default: {}
       attribute :showtime, :integer, default: 0
       attribute :accept_email, :boolean, default: true
